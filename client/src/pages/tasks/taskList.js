a6d4fe7f72bb2342d8f3f901ac6ffa3d72ee384d
Ting-Yeh Lin <s88432000@hotmail.com>
2021-10-04 01:15:37 -0400

    can display tasklist

--- client/src/pages/tasks/taskList.js
+++ client/src/pages/tasks/taskList.js
@@ -1,12 +1,26 @@
- 
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
+ import {  ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
-export default class TaskList extends Component {
+export default class TaskList extends Component {    
     render() {
+        const tasks = [
+            { date: '2021-10-08', eventName: 'My Birtheday' },
+            { date: '2021-10-30', eventName: 'The end of study week' },
+            { date: '2021-10-24', eventName: 'Start of study week' },
+            { date: '2021-10-08', eventName: 'PMC115 Quiz' }
+        ];
+        let sorted = tasks.sort((a, b) =>
+            a.date.split('-').reverse().join().localeCompare(b.date.split('-').reverse().join()));
         return (
-            <div>
-                <h2>Contact</h2>
-            </div>
+            <>
+                <h2>Task List</h2>
+                <ListGroup>
+                    {sorted.map(task => (
+                        <ListGroupItem href="task/:id">{task.date}  {task.eventName}</ListGroupItem>
+                    ))}
+                </ListGroup>
+                <Button href="#">Create New Task</Button>
+            </>
         )
     }
 }
