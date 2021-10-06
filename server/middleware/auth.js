99fe5b1712dba2cf6384457874bde195850300fb
klee214 <klee214@myseneca.ca>
2021-10-05 20:19:46 -0400

    firebase config

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+const admin = require("../firebase/index");
+const User = require("../models/user");
+
+exports.authCheck = async (req, res, next) => {
+  try {
+    const firebaseUser = await adminls
+      .auth()
+      .verifyIdToken(req.headers.authtoken);
+    req.user = firebaseUser;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401);
+    res.json({
+      err: "The token is invalid or expired",
+    });
+  }
+};
