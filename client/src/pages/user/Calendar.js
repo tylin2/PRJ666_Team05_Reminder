55b334adaf69bace4e3a639bb0007f8a2f55b0ba
Ting-Yeh Lin <s88432000@hotmail.com>
2021-10-08 18:23:21 -0400

    Create Calendar.js

---
+++ client/src/pages/user/Calendar.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'  
+import FullCalendar from "@fullcalendar/react";  
+import dayGridPlugin from "@fullcalendar/daygrid";  
+
+const events = [{ title: "Today", date: new Date() }];  
+export class Calendar extends Component {  
+    render() {  
+        return (  
+            <div className="container">  
+                <div className="row title" style={{ marginTop: "20px" }} >  
+                    <div class="col-sm-12 btn btn-info">  
+                        FullCalendar In React Application  
+               </div>  
+                </div>  
+                <FullCalendar  
+                    defaultView="dayGridMonth"  
+                    plugins={[dayGridPlugin]}  
+                    events={events}  
+                />  
+            </div>  
+        )  
+    }  
+}  
+  
+export default Calendar 
