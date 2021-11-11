13baaa0d4ebbaf0c2b11a3120ae4e4b759109f12
klee214 <klee214@myseneca.ca>
2021-11-10 22:03:23 -0500

    added: verify sendgrid can send image file and other css

--- server/functions/sendgrid/send_email.js
+++ server/functions/sendgrid/send_email.js
@@ -3,12 +3,13 @@ const sgMail = require("@sendgrid/mail");
 require("dotenv").config({ path: "../../.env" });
 
 const send_email_function = async (tasks) => {
+  console.log(tasks);
   sgMail.setApiKey(`${process.env.SENDGRID_API_KEY}`);
   const msg = {
     to: "dlrlals05050@gmail.com", // Change to your recipient
     from: "klee214@myseneca.ca", // Change to your verified sender
     subject: "DEMO",
-    text: "The demo is working now",
+    html: `<img src="https://inapak.com/wp-content/uploads/2017/07/iStock-491520707.jpg"><h1>${tasks[0]}The demo is working now</h1>`,
   };
 
   sgMail
--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ dotenv.config();
 
 // app
 const app = express();
+app.use(express.static('public'))
 
 //db
 mongoose
